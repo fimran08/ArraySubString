4c4aaadbbe26ba2e472673f981ffd2b9fa458aa1
fimran08 <fatima_imran@hotmail.co.uk>
2021-12-01 13:55:24 +0000

    Initial commit

---
+++ Ten10/ArraySubstringCount/SortingTests.cs
@@ -0,0 +1,19 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace ArraySubstringCount
+{
+    public class SortingTests
+    {
+        [Theory]
+        [InlineData(new[] { 1, 4, 1, 4, 2, 1, 3, 5, 6, 2, 3, 7 }, 4)]
+        //[InlineData(new[] { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5 }, 3)]
+        //[InlineData(new[] { 2, 7, 1, 8, 2, 8, 1 }, 2)]
+        //[InlineData(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 10)]
+        //[InlineData(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 1 }, 9)]
+        public void RunSubSequenceCounter(int[] array, int expectedOutput)
+        {
+            SubSequenceHelper.SubSequenceCounter(array).ShouldBe(expectedOutput);
+        }
+    }
+}
---
+++ Ten10/ArraySubstringCount/SubSequenceHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraySubstringCount
+{
+    public static class SubSequenceHelper
+    {
+        public static int SubSequenceCounter(IReadOnlyList<int> array)
+        {
+            // the result of the function
+            var result = 0;
+
+            // current position
+            var current = 0;
+
+            // where the position in the array is 0, when the position is less than the length of the array increment the position 
+            for (var position = 0; position < array.Count; position++)
+            {
+                // if the position in the array is greater than 0 AND value of the current position is less than or equal to current position then minus 1
+                if (position > 0 && array[position] <= array[position - 1])
+
+                    // then reset the current position
+                    current = 0;
+
+                // if the above is not true increment the current position by 1
+                current++;
+
+                // set the result to the value of whichever variable is higher between the result and the current position
+                result = Math.Max(result, current);
+            }
+
+            // return the result
+            return result;
+        }
+    }
+}
